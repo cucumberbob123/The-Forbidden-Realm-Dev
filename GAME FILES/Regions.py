19f6bb3a1f0cfa203155e96e5ae6dba405d97449
thomasnoakes04 <45767072+thomasnoakes04@users.noreply.github.com>
2018-12-11 19:54:30 +0000

    Update Regions.py

--- GAME FILES/Regions.py
+++ GAME FILES/Regions.py
@@ -3,7 +3,16 @@ class region():
         self.place = place
     def describe(self):
         if self.place == "forest":
-            print("THE FOREST:\nFull of trees.\n\nResources:\nWood,\nWild"
+            print("THE FOREST:\nFull of trees.\n\nResources:\nWood,\nWild "
                   "Berry,\nStick,\nGrass\n\nAnimals:\nDeer,\nBird,\nFox")
-forest = region()
-forest.describe("forest")
+        if self.place == "river":
+            print("THE RIVER:\nA steady stream of water.\n\nResources:\n"
+                  "Water,\nClay,\nReeds\n\nAnimals:\nSalmon,\nCod")
+        if self.place == "mountains":
+            print("THE MOUNTAINS:\nA giant hunk of rock.\n\nResources:\n"
+                  "Rock,\nFlint,\nSpring Water\n\nAnimals:\nEagle,\nMountain Goat")
+
+forest = region("forest")
+river = region("river")
+mountains = region("mountains")
+mountains.describe()
